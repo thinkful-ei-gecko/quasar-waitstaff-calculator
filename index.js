6a0999a360de90196dd85c9b8b7085cc0c7df2a0
Quasar Wei <quasar.wei@outlook.com>
2019-08-20 22:03:03 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const STORE = {
+  meals: [{price: 10.00, 
+    taxRate: 0.045, 
+    tipPercentage: 0.15, 
+    subtotal: 0.0, 
+    tip: 0.0}],
+  tipTotal: 0,
+  averageTip: 0
+}
+
